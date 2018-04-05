8e1e45c091a2a649b3b6b178f615b75545065e1f
Brian Peters <BrianRPeters@gmail.com>
2018-04-05 14:19:46 -0500

    Add files via upload

---
+++ afk.ps1
@@ -0,0 +1,21 @@
+﻿param($minutes = 240)
+
+$pshost = Get-Host
+$pswindow = $pshost.UI.RawUI
+$newsize = $pswindow.BufferSize
+$newsize.Width = 150
+$newsize.Height = 150
+$pswindow.buffersize= $newsize
+$newsize = $pswindow.WindowSize
+$newsize.Width = 9
+$newsize.Height = 2
+$pswindow.WindowSize = $newsize
+
+$myshell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $minutes; $i++){
+    cls
+    Write-Host "Anti-AFK"
+    Start-Sleep -Seconds 60
+    $myshell.sendkeys("{F17}")
+}
